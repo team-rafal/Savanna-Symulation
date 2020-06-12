3906352e011d2f80081ed820de5a8f5ea0854613
PewPewBartula <63607789+PewPewBartula@users.noreply.github.com>
2020-06-12 14:39:59 +0200

    Update Antelope.java

--- PROJEKT/src/animals/Antelope.java
+++ PROJEKT/src/animals/Antelope.java
@@ -9,7 +9,7 @@ import world.Map;
  /** 
  * @author Andrzej Olszewski, Bartosz Szymczak, Rafa³ Rodak, Mateusz Marciniec.
  * 
- * @version 0.4.0
+ * @version 1.0.0
  */
 
 public class Antelope extends Animal{
