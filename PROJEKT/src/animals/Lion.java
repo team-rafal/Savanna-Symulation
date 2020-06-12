80b97025c7f526951c12136c832de8b3a2f8b6ba
PewPewBartula <63607789+PewPewBartula@users.noreply.github.com>
2020-06-12 14:41:02 +0200

    Update Lion.java

--- PROJEKT/src/animals/Lion.java
+++ PROJEKT/src/animals/Lion.java
@@ -10,7 +10,7 @@ import world.Map;
  /** 
  * @author Andrzej Olszewski, Bartosz Szymczak, Rafa³ Rodak, Mateusz Marciniec.
  * 
- * @version 0.4.0
+ * @version 1.0.0
  */
 
 public class Lion extends Animal{
